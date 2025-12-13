6f8c4293e52b686afe581a41a4ee59d16eac9f3c
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2025-12-13 17:13:50 +0000

    Add .env to .gitignore; update main.js to include user and task models and enhance user prompt functionality

--- script/main.js
+++ script/main.js
@@ -1,7 +1,36 @@
 import { APIClient } from "./api.js";
+import { Task, User } from "./models.js";
 
-const client = new APIClient();
+const apiClient = new APIClient();
 
-console.log("users",client.fetchUser());
-console.log("todoo", client.fetchTodos());
-console.log("usertod", client.fetchUserTodos())
+async function main() {
+  const [usersData, todosData] = await Promise.all([
+    apiClient.fetchUser(),
+    apiClient.fetchTodos(),
+  ]);
+
+  const users = usersData.map((user) => new User(user));
+  const tasks = todosData.map((todos) => new Task(todos));
+  console.log(tasks);
+
+  // PromptUser(users, tasks);
+
+  function PromptUser(users, tasks) {
+    let loading = true;
+
+    while (loading) {
+      const choice = prompt(`
+              ===== Task Manager API Client =====
+          \n1. Show all tasks
+          \n2. Show completed tasks
+          \n3. Show pending tasks
+          \n4. Show user statistics
+          \n5. Show tasks for a user
+          \n6. Exit
+          Choose an option:
+              `);
+    }
+  }
+}
+
+main();
