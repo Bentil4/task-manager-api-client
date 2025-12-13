3bb670239719b80e56e79b9a3645c2c6941e6cbe
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2025-12-13 15:28:00 +0000

    Implement groupByUser function to organize tasks by user ID

--- script/taskProcessor.js
+++ script/taskProcessor.js
@@ -17,4 +17,12 @@ export const calculateStatistics = (tasks) => {
   );
 };
 
-export const groupByUser = (tasks) => {};
+export const groupByUser = (tasks) => {
+    const map = new Map()
+    tasks.forEach(task => {
+        if (!map.has(task.userId)){
+            map.set(task.userId, []);
+        }
+        map.get(task.userId).push(task);
+    })
+};
