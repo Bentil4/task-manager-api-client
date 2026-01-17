65ae3c65cd9c293697a956bb0db8edd8093ed619
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-01-17 12:25:11 +0000

    add test coverage folder to gitignore, and make changes to jest.config.js and package.json files

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
-// jest.config.js (ESM)
 export default {
   testEnvironment: "node",
-  // No transforms — we’re not using Babel
   transform: {},
-  // DO NOT add extensionsToTreatAsEsm when "type": "module" is set
-  // extensionsToTreatAsEsm: ['.js'], // ❌ remove
-
-  // Optional: helps with imports that include .js extensions
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
-
-  // Optional: if you want to explicitly pick up both test/ and tests/
-  // testMatch: ['**/test/**/*.test.js', '**/tests/**/*.test.js'],
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["script/*.js"],
+  injectGlobals: true,
+  coveragePathIgnorePatterns: ["script/main.js"],
 };
 ``;
